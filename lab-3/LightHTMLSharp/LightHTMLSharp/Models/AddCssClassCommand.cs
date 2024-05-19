39ea1891232e088f3cee2c99fc8bb9154418605b
FrontGuard <158158658+FrontGuard@users.noreply.github.com>
2024-05-19 04:33:39 +0300

    Ще одна фіча

---
+++ lab-3/LightHTMLSharp/LightHTMLSharp/Models/AddCssClassCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LightHTMLSharp.Models
+{
+    public class AddCssClassCommand : ICommand
+    {
+        private LightElementNode _element;
+        private string _cssClass;
+
+        public AddCssClassCommand(LightElementNode element, string cssClass)
+        {
+            _element = element;
+            _cssClass = cssClass;
+        }
+
+        public void Execute()
+        {
+            _element.CssClasses.Add(_cssClass);
+        }
+
+        public void Undo()
+        {
+            _element.CssClasses.Remove(_cssClass);
+        }
+    }
+}
